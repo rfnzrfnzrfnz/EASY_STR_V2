660ccde426b0acaac95380c30da31b7cb8c7f041
катя <45289493+rfnzrfnzrfnz@users.noreply.github.com>
2020-10-22 12:20:03 +0300

    Update ft_cut_between_char.py

--- ft_cut_between_char.py
+++ ft_cut_between_char.py
@@ -1,43 +1,50 @@
-def ft_len(str):
-    l = 0
-    for i in str:
-        l += 1
-    return (l)
+def ft_count_char_in_str(d, b):
+    c = 0
+    for i in b:
+        if i == d:
+            c += 1
+    return c
 
 
-def ft_cut_between_char(char, str):
+def ft_len(b):
+    c = 0
+    for _ in b:
+        c += 1
+    return c
+
+
+def ft_find_char(d, b):
+    if d not in b:
+        return False
+    for i in range(ft_len(b)):
+        if b[i] == d:
+            return i + 1
+
+
+def ft_find_second_char(d, b):
     x = 0
-    r = 0
-    w = ''
-    count = 0
-    if char in str:
-        for i in str:
-            if i == char:
-                count += 1
-
-        if count == 1:
-            return -1
-
+    if ft_count_char_in_str(d, b) == 1:
+        return -1
+    elif ft_count_char_in_str(d, b) == 0:
+        return False
+    for i in range(ft_len(b)):
+        if b[i] == d:
+            x += 1
+            if x == 2:
+                return i
 
 
-        else:
-            for i in range(ft_len(str)):
-                if str[i] == char:
-                    x = i
-
-                    break
-            for i in range(ft_len(str)):
-                if str[i] == char:
-                    r = i
-            for i in range(x):
-                w += str[i]
-
-            r += 1
-            while r < ft_len(str):
-                w += str[r]
-                r += 1
-            return w
+def ft_slice_str(b, st, sp):
+    r = ''
+    for i in range(st, sp):
+        r += b[i]
+    return r
 
 
-    else:
+def ft_cut_between_char(d, b):
+    if ft_count_char_in_str(d, b) == 1:
+        return -1
+    elif ft_count_char_in_str(d, b) == 0:
         return -2
+    return ft_slice_str(b, 0, ft_find_char(d, b) - 1) + \
+        ft_slice_str(b, ft_find_second_char(d, b) + 1, ft_len(b))
