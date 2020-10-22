211d7908d078f48fe528cd3e3ad07fd0d4b13cdd
катя <45289493+rfnzrfnzrfnz@users.noreply.github.com>
2020-10-22 12:34:52 +0300

    Update ft_count_words.py

--- ft_count_words.py
+++ ft_count_words.py
@@ -1,6 +1,25 @@
-def ft_count_words(str):
-    count = 1
+  
+def ft_len(str):
+    a = 0
     for i in str:
-        if i == ' ':
-            count += 1
-    return count
+        a += 1
+    return (a)
+
+
+def ft_count_words(str):
+    k = 0
+    d = ft_len(str)
+    i = 0
+    if str[0] == ' ':
+        while i < d:
+            if str[i] == ' ' and str[i - 1] != ' ':
+                k = k + 1
+            i = i + 1
+        return k
+    else:
+        while i < d:
+            if str[i] == ' ' and str[i - 1] != ' ':
+                k = k + 1
+            i = i + 1
+        return k + 1
+    
