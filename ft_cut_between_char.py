5252d3a43d02aee650c58762b4e86ce20d60e3cc
катя <45289493+rfnzrfnzrfnz@users.noreply.github.com>
2020-10-22 12:39:46 +0300

    Update ft_cut_between_char.py

--- ft_cut_between_char.py
+++ ft_cut_between_char.py
@@ -47,20 +47,4 @@ def ft_cut_between_char(d, b):
     elif ft_count_char_in_str(d, b) == 0:
         return -2
     return ft_slice_str(b, 0, ft_find_char(d, b) - 1) + \
-        ft_slice_str(b, ft_find_second_char(d, b) + 1, ft_len(b))
-
-            
-def ft_slice_str(b, st, sp):
-    r = ''
-    for i in range(st, sp):
-        r += b[i]
-    return r
-
-
-def ft_cut_between_char(d, b):
-    if ft_count_char_in_str(d, b) == 1:
-        return -1
-    elif ft_count_char_in_str(d, b) == 0:
-        return -2
-    return ft_slice_str(b, 0, ft_find_char(d, b) - 1) + \
-        ft_slice_str(b, ft_find_second_char(d, b) + 1, ft_len(b))
+           ft_slice_str(b, ft_find_second_char(d, b) + 1, ft_len(b))
