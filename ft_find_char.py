455f76c521402f005d1360158d26ae99e2dca5a2
катя <45289493+rfnzrfnzrfnz@users.noreply.github.com>
2020-10-22 12:33:07 +0300

    Update ft_find_char.py

--- ft_find_char.py
+++ ft_find_char.py
@@ -1,32 +1,28 @@
-def ft_len(str):
+def ft_len(sstr):
     l = 0
-    for i in str:
+    for i in sstr:
         l += 1
-    return (l)
+    return l
 
 
-def ft_find_char(char, str):
+def ft_find_char(char, sstr):
     count = 0
     x = 0
     r = 0
-    if char in str:
-        for i in range(ft_len(str)):
-            if str[i] == char:
+    if char in sstr:
+        for i in range(ft_len(sstr)):
+            if sstr[i] == char:
                 x = i
-
                 break
-
-        for i in str:
+        for i in sstr:
             if i == char:
                 count += 1
         if count == 1:
             return x
         else:
-            for i in range(ft_len(str)):
-                if str[i] == char:
+            for i in range(ft_len(sstr)):
+                if sstr[i] == char:
                     r = i
             return x, r
-
-
     else:
         return False
